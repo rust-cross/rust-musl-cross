3f212e585f9f7e59ce629247f95eee1b2282a9ab
messense <messense@icloud.com>
2017-10-11 23:25:13 +0800

    Fix build.sh and add Travis CI intergration

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ docker build -t messense/rust-musl-cross:x86_64-musl .
 # arm-unknown-linux-musleabi
 docker build --build-arg TARGET=arm-unknown-linux-musleabi --build-arg OPENSSL_ARCH=linux-generic32 -t messense/rust-musl-cross:arm-musleabi .
 # arm-unknown-linux-musleabihf
-docker build --build-arg TARGET=arm-unknown-linux-musleabihf --build-arg OPENSSL_ARCH=linux-generic32 -t messense/rust-musl-cross:arm-musleabi .
+docker build --build-arg TARGET=arm-unknown-linux-musleabihf --build-arg OPENSSL_ARCH=linux-generic32 -t messense/rust-musl-cross:arm-musleabihf .
 # armv7-unknown-linux-musleabihf
-docker build --build-arg TARGET=armv7-unknown-linux-musleabihf --build-arg OPENSSL_ARCH=linux-generic32 -t messense/rust-musl-cross:armv7-musleabi .
+docker build --build-arg TARGET=armv7-unknown-linux-musleabihf --build-arg OPENSSL_ARCH=linux-generic32 -t messense/rust-musl-cross:armv7-musleabihf .
 # i686-unknown-linux-musl
 docker build --build-arg TARGET=i686-unknown-linux-musl --build-arg OPENSSL_ARCH=linux-generic32 -t messense/rust-musl-cross:i686-musl .
 # mips-unknown-linux-musl
