edc1eb635fbb6feca12502dbb8eeb75a03d1c434
messense <messense@icloud.com>
2017-10-12 20:25:21 +0800

    Add cross compile integration tests
    Fixes #2

---
+++ tests/src/main.rs
@@ -0,0 +1,36 @@
+#![allow(warnings)] // remove when error_chain is fixed
+
+//! `cargo run --example simple`
+
+extern crate reqwest;
+extern crate env_logger;
+#[macro_use]
+extern crate error_chain;
+extern crate openssl_probe;
+
+error_chain! {
+    foreign_links {
+        ReqError(reqwest::Error);
+        IoError(std::io::Error);
+    }
+}
+
+fn run() -> Result<()> {
+    env_logger::init().expect("Failed to initialize logger");
+    openssl_probe::init_ssl_cert_env_vars();
+
+    println!("GET https://www.rust-lang.org");
+
+    let mut res = reqwest::get("https://www.rust-lang.org/en-US/")?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{}", res.headers());
+
+    // copy the response body directly to stdout
+    let _ = std::io::copy(&mut res, &mut std::io::stdout())?;
+
+    println!("\n\nDone.");
+    Ok(())
+}
+
+quick_main!(run);
