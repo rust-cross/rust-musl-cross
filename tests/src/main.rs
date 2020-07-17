bc4f58580880e749544725939752279e800b6d34
messense <messense@icloud.com>
2020-07-17 14:00:27 +0800

    Update tests crate dependencies

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -21,10 +21,10 @@ fn run() -> Result<()> {
 
     println!("GET https://www.rust-lang.org");
 
-    let mut res = reqwest::get("https://www.rust-lang.org/en-US/")?;
+    let mut res = reqwest::blocking::get("https://www.rust-lang.org/en-US/")?;
 
     println!("Status: {}", res.status());
-    println!("Headers:\n{}", res.headers());
+    println!("Headers:\n{:?}", res.headers());
 
     // copy the response body directly to stdout
     let _ = std::io::copy(&mut res, &mut std::io::stdout())?;
