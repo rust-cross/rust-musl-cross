fb3f67eda4cb83896109a38cda56ada202800233
messense <messense@icloud.com>
2021-03-23 20:09:58 +0800

    Add i585 build to build.sh [ci skip]

--- build.sh
+++ build.sh
@@ -14,7 +14,9 @@ docker build --build-arg TARGET=armv5te-unknown-linux-musleabi --build-arg OPENS
 # armv7-unknown-linux-musleabihf
 docker build --build-arg TARGET=armv7-unknown-linux-musleabihf --build-arg OPENSSL_ARCH=linux-generic32 --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t messense/rust-musl-cross:armv7-musleabihf .
 # i686-unknown-linux-musl
-docker build --build-arg TARGET=i686-unknown-linux-musl --build-arg OPENSSL_ARCH=linux-generic32 --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t messense/rust-musl-cross:i686-musl .
+docker build --build-arg TARGET=i686-unknown-linux-musl --build-arg OPENSSL_ARCH=linux-elf --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t messense/rust-musl-cross:i686-musl .
+# i586-unknown-linux-musl
+docker build --build-arg TARGET=i586-unknown-linux-musl --build-arg OPENSSL_ARCH=linux-elf --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t messense/rust-musl-cross:i586-musl .
 # mips-unknown-linux-musl
 docker build --build-arg TARGET=mips-unknown-linux-musl --build-arg OPENSSL_ARCH=linux-mips32 --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t messense/rust-musl-cross:mips-musl .
 # mipsel-unknown-linux-musl
